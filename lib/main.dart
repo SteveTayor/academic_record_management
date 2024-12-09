c61c2fb2b77b486ca96a9264d5f2082c2f14dfae
SteveTayor <ayomidearose75@gmail.com>
2024-12-09 17:21:54 +0100

    configs

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,6 @@ void main() async {
   // Flutter Widgets.
   final settingsController = SettingsController(SettingsService());
 
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
   runApp(MyApp(settingsController: settingsController));
 }
