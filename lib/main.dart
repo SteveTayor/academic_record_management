7afbd2cb90942d3d03c28f8e87ccf31471161d1c
SteveTayor <ayomidearose75@gmail.com>
2024-12-16 23:15:42 +0100

    `Added Firebase configuration and dependencies to the project`

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,16 @@ import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
   final settingsController = SettingsController(SettingsService());
 
   await settingsController.loadSettings();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp(settingsController: settingsController));
 }
